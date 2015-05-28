31d9f975f566f1efd263e524d57cb58acb1f7a45
Jeffrey Johnson <jjphatt@gmail.com>
2015-05-28 11:25:54 -0700

    Forgot to add grid_data files.

---
+++ polyamri/amr_grid_data.c
@@ -0,0 +1,135 @@
+// Copyright (c) 2014-2015, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "polyamri/amr_grid_data.h"
+
+struct amr_grid_data_t
+{
+  amr_grid_t* grid;
+  int nx, ny, nz;
+  void* patches;
+  void* bboxes;
+};
+
+// Multi-dimensional array accessors for patches, bboxes.
+#define DECLARE_PATCH_ARRAY(array, grid_data) \
+amr_patch_t* (*array)[grid_data->nx][grid_data->ny] = grid_data->patches
+
+#define DECLARE_BBOX_ARRAY(array, grid_data) \
+bbox_t* (*array)[grid_data->nx][grid_data->ny] = grid_data->bboxes
+
+amr_grid_data_t* amr_grid_data_new(amr_grid_t* grid, int num_components)
+{
+  ASSERT(num_components > 0);
+
+  // Allocate storage.
+  int num_patches = amr_grid_num_patches(grid);
+  size_t patches_size = sizeof(amr_patch_t*) * num_patches;
+  size_t bboxes_size = sizeof(bbox_t*) * num_patches;
+  size_t grid_data_size = sizeof(amr_grid_data_t) + patches_size + bboxes_size;
+  amr_grid_data_t* grid_data = polymec_malloc(grid_data_size);
+  grid_data->grid = grid;
+  amr_grid_get_extents(grid, &grid_data->nx, &grid_data->ny, &grid_data->nz);
+  grid_data->patches = (char*)grid_data + sizeof(amr_grid_data_t);
+  memset(grid_data->patches, 0, patches_size);
+  grid_data->bboxes = (char*)grid_data + sizeof(amr_grid_data_t) + patches_size;
+  memset(grid_data->bboxes, 0, bboxes_size);
+
+  // Now populate the patches/bboxes.
+  bbox_t* domain = amr_grid_domain(grid);
+  int px, py, pz, ng;
+  amr_grid_get_patch_size(grid, &px, &py, &pz, &ng);
+  DECLARE_PATCH_ARRAY(patches, grid_data);
+  DECLARE_BBOX_ARRAY(bboxes, grid_data);
+  real_t dx = (domain->x2 - domain->x1) / grid_data->nx;
+  real_t dy = (domain->y2 - domain->y1) / grid_data->ny;
+  real_t dz = (domain->z2 - domain->z1) / grid_data->nz;
+  for (int i = 0; i < grid_data->nx; ++i)
+  {
+    for (int j = 0; j < grid_data->ny; ++j)
+    {
+      for (int k = 0; k < grid_data->nz; ++k)
+      {
+        if (amr_grid_has_patch(grid, i, j, k))
+        {
+          patches[i][j][k] = amr_patch_new(px, py, pz, num_components, ng);
+          bboxes[i][j][k] = bbox_new(domain->x1 + i*dx, 
+                                     domain->x1 + (i+1)*dx,
+                                     domain->y1 + j*dx, 
+                                     domain->y1 + (j+1)*dy,
+                                     domain->z1 + k*dz, 
+                                     domain->z1 + (k+1)*dz);
+        }
+      }
+    }
+  }
+
+
+  return grid_data;
+}
+
+void amr_grid_data_free(amr_grid_data_t* grid_data)
+{
+  DECLARE_PATCH_ARRAY(patches, grid_data);
+  DECLARE_BBOX_ARRAY(bboxes, grid_data);
+  for (int i = 0; i < grid_data->nx; ++i)
+  {
+    for (int j = 0; j < grid_data->ny; ++j)
+    {
+      for (int k = 0; i < grid_data->nz; ++k)
+      {
+        if (patches[i][j][k] != NULL)
+          amr_patch_free(patches[i][j][k]);
+        if (bboxes[i][j][k] != NULL)
+          bboxes[i][j][k] = NULL;
+      }
+    }
+  }
+  polymec_free(grid_data);
+}
+
+int amr_grid_data_num_patches(amr_grid_data_t* grid_data)
+{
+  return amr_grid_num_patches(grid_data->grid);
+}
+
+amr_grid_t* amr_grid_data_grid(amr_grid_data_t* grid_data)
+{
+  return grid_data->grid;
+}
+
+amr_patch_t* amr_grid_data_patch(amr_grid_data_t* grid_data, int i, int j, int k, bbox_t** bbox)
+{
+  DECLARE_PATCH_ARRAY(patches, grid_data);
+  DECLARE_BBOX_ARRAY(bboxes, grid_data);
+  if (bbox != NULL)
+    *bbox = bboxes[i][j][k];
+  return patches[i][j][k];
+}
+
+bool amr_grid_data_next(amr_grid_data_t* grid_data, int* pos, 
+                        int* i, int* j, int* k, 
+                        amr_patch_t** patch, bbox_t** bbox)
+{
+  int index = *pos;
+  int num_patches = amr_grid_num_patches(grid_data->grid);
+  amr_patch_t** patches = grid_data->patches;
+  if (index < num_patches)
+  {
+    *patch = patches[index];
+    if (bbox != NULL)
+    {
+      bbox_t** bboxes = grid_data->bboxes;
+      *bbox = bboxes[index];
+    }
+    while ((patches[index] == NULL) && (index < num_patches))
+      ++(*pos);
+    return true;
+  }
+  return false;
+}
+
---
+++ polyamri/amr_grid_data.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2014-2015, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef POLYAMRI_AMR_GRID_DATA_H
+#define POLYAMRI_AMR_GRID_DATA_H
+
+#include "core/point.h"
+#include "polyamri/amr_grid.h"
+#include "polyamri/amr_patch.h"
+
+// An AMR grid data object is a collection of patches that are associated 
+// with an amr_grid in 3D space.
+typedef struct amr_grid_data_t amr_grid_data_t;
+
+// Creates an amr_grid_data object with the given number of components, 
+// associated with the given amr_grid.
+amr_grid_data_t* amr_grid_data_new(amr_grid_t* grid, int num_components);
+
+// Frees the given amr_grid_data object.
+void amr_grid_data_free(amr_grid_data_t* grid_data);
+
+// Returns the number of (local) patches in the amr_grid_data object.
+int amr_grid_data_num_patches(amr_grid_data_t* grid_data);
+
+// Returns an internal pointer to the given object's underlying amr_grid.
+amr_grid_t* amr_grid_data_grid(amr_grid_data_t* grid_data);
+
+// Given a tuple (i, j, k) identifying a patch in the underlying amr_grid,
+// returns a patch containing data, or NULL if the patch is not present 
+// locally on the amr_grid. If bbox is non-NULL, it is set to the bounding 
+// box for the patch.
+amr_patch_t* amr_grid_data_patch(amr_grid_data_t* grid_data, int i, int j, int k, bbox_t** bbox);
+
+// Traverses the grid data, returning true if a patch was found and false if not.
+// Set *pos to 0 to reset the traversal. Pointers to the patch and its 
+// bounding box are returned in the given locations.
+bool amr_grid_data_next(amr_grid_data_t* grid_data, int* pos, 
+                        int* i, int* j, int* k, 
+                        amr_patch_t** patch, bbox_t** bbox);
+
+#endif
+
