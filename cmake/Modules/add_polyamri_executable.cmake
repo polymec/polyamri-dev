1bc0c5bf0588a64bdfc69f5d1f4cfad390949ad0
Jeffrey Johnson <jjphatt@gmail.com>
2015-11-19 15:52:49 -0800

    Got rid of useless comment.

--- cmake/Modules/add_polyamri_executable.cmake
+++ cmake/Modules/add_polyamri_executable.cmake
@@ -1,4 +1,3 @@
-# This function adds a (serial) unit test executable to be built using cmockery.
 function(add_polyamri_executable exe)
   add_executable(${exe} ${ARGN})
   target_link_libraries(${exe} ${POLYAMRI_LIBRARIES})
