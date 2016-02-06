8d88a17c3c68b08b427079563c3ebbae03696ce5
Jeffrey Johnson <jjphatt@gmail.com>
2016-02-05 21:59:13 -0800

    Added edge-centered data.

---
+++ polyamri/str_grid_edge_data.c
@@ -0,0 +1,252 @@
+// Copyright (c) 2014-2016, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "core/unordered_map.h"
+#include "polyamri/str_grid_edge_data.h"
+
+struct str_grid_edge_data_t 
+{
+  str_grid_t* grid;
+  int nx, ny, nz, nc;
+  int_ptr_unordered_map_t* x_patches;
+  int_ptr_unordered_map_t* y_patches;
+  int_ptr_unordered_map_t* z_patches;
+  int* patch_offsets;
+  int num_edges;
+
+  void* buffer;
+  bool owns_buffer;
+};
+
+static inline int patch_index(str_grid_edge_data_t* edge_data, int i, int j, int k)
+{
+  return edge_data->ny*edge_data->nz*i + edge_data->nz*j + k;
+}
+
+static void count_edges(str_grid_edge_data_t* edge_data)
+{
+  int pos = 0, ip, jp, kp, l = 1;
+  str_grid_patch_t* patch;
+  edge_data->num_edges = 0;
+  edge_data->patch_offsets[0] = 0;
+  while (str_grid_edge_data_next_x_patch(edge_data, &pos, &ip, &jp, &kp, &patch))
+  {
+    int nx = patch->i2 - patch->i1;
+    int ny = patch->j2 - patch->j1 + 1;
+    int nz = patch->k2 - patch->k1 + 1;
+    edge_data->num_edges += nx * ny * nz;
+    edge_data->patch_offsets[l] = edge_data->num_edges * patch->nc;
+    ++l;
+  }
+
+  pos = 0;
+  while (str_grid_edge_data_next_y_patch(edge_data, &pos, &ip, &jp, &kp, &patch))
+  {
+    int nx = patch->i2 - patch->i1 + 1;
+    int ny = patch->j2 - patch->j1;
+    int nz = patch->k2 - patch->k1 + 1;
+    edge_data->num_edges += nx * ny * nz;
+    edge_data->patch_offsets[l] = edge_data->num_edges * patch->nc;
+    ++l;
+  }
+
+  pos = 0;
+  while (str_grid_edge_data_next_z_patch(edge_data, &pos, &ip, &jp, &kp, &patch))
+  {
+    int nx = patch->i2 - patch->i1 + 1;
+    int ny = patch->j2 - patch->j1 + 1;
+    int nz = patch->k2 - patch->k1;
+    edge_data->num_edges += nx * ny * nz;
+    edge_data->patch_offsets[l] = edge_data->num_edges * patch->nc;
+    ++l;
+  }
+}
+
+str_grid_edge_data_t* str_grid_edge_data_new(str_grid_t* grid, 
+                                             int num_components)
+{
+  str_grid_edge_data_t* edge_data = 
+    str_grid_edge_data_with_buffer(grid, num_components, NULL);
+  int data_size = edge_data->num_edges * edge_data->nc;
+  void* buffer = polymec_malloc(sizeof(real_t) * data_size);
+  str_grid_edge_data_set_buffer(edge_data, buffer, true);
+  return edge_data;
+}
+
+str_grid_edge_data_t* str_grid_edge_data_with_buffer(str_grid_t* grid, 
+                                                     int num_components,
+                                                     void* buffer)
+{
+  ASSERT(num_components > 0);
+
+  // Allocate storage.
+  int num_patches = 3 * str_grid_num_patches(grid);
+  size_t patches_size = sizeof(str_grid_patch_t*) * num_patches;
+  size_t edge_data_size = sizeof(str_grid_edge_data_t) + patches_size;
+  str_grid_edge_data_t* edge_data = polymec_malloc(edge_data_size);
+  edge_data->grid = grid;
+  edge_data->nc = num_components;
+
+  str_grid_get_extents(grid, &edge_data->nx, &edge_data->ny, &edge_data->nz);
+  edge_data->x_patches = int_ptr_unordered_map_new();
+  edge_data->y_patches = int_ptr_unordered_map_new();
+  edge_data->z_patches = int_ptr_unordered_map_new();
+  edge_data->patch_offsets = polymec_malloc(sizeof(int) * (3*num_patches+1));
+
+  // Now populate the patches for x-, y-, and z-edges.
+  int px, py, pz;
+  str_grid_get_patch_size(grid, &px, &py, &pz);
+  int pos = 0, i, j, k;
+  while (str_grid_next_patch(grid, &pos, &i, &j, &k))
+  {
+    int index = patch_index(edge_data, i, j, k);
+
+    int_ptr_unordered_map_insert_with_v_dtor(edge_data->x_patches, index, 
+                                             str_grid_patch_with_buffer(px+1, py, pz, num_components, 0, NULL),
+                                             DTOR(str_grid_patch_free));
+
+    int_ptr_unordered_map_insert_with_v_dtor(edge_data->y_patches, index, 
+                                             str_grid_patch_with_buffer(px, py+1, pz, num_components, 0, NULL),
+                                             DTOR(str_grid_patch_free));
+
+    int_ptr_unordered_map_insert_with_v_dtor(edge_data->z_patches, index, 
+                                             str_grid_patch_with_buffer(px, py, pz+1, num_components, 0, NULL),
+                                             DTOR(str_grid_patch_free));
+  }
+
+  // Set the buffer.
+  str_grid_edge_data_set_buffer(edge_data, buffer, false);
+
+  return edge_data;
+}
+
+void str_grid_edge_data_free(str_grid_edge_data_t* edge_data)
+{
+  int_ptr_unordered_map_free(edge_data->x_patches);
+  int_ptr_unordered_map_free(edge_data->y_patches);
+  int_ptr_unordered_map_free(edge_data->z_patches);
+  if (edge_data->owns_buffer)
+    polymec_free(edge_data->buffer);
+  polymec_free(edge_data);
+}
+
+int str_grid_edge_data_num_components(str_grid_edge_data_t* edge_data)
+{
+  return edge_data->nc;
+}
+
+int str_grid_edge_data_num_edges(str_grid_edge_data_t* edge_data)
+{
+  return edge_data->num_edges;
+}
+
+str_grid_t* str_grid_edge_data_grid(str_grid_edge_data_t* edge_data)
+{
+  return edge_data->grid;
+}
+
+str_grid_patch_t* str_grid_edge_data_x_patch(str_grid_edge_data_t* edge_data, int i, int j, int k)
+{
+  int index = patch_index(edge_data, i, j, k);
+  str_grid_patch_t** pp = (str_grid_patch_t**)int_ptr_unordered_map_get(edge_data->x_patches, index);
+  if (pp != NULL)
+    return *pp;
+  else
+    return NULL;
+}
+
+str_grid_patch_t* str_grid_edge_data_y_patch(str_grid_edge_data_t* edge_data, int i, int j, int k)
+{
+  int index = patch_index(edge_data, i, j, k);
+  str_grid_patch_t** pp = (str_grid_patch_t**)int_ptr_unordered_map_get(edge_data->y_patches, index);
+  if (pp != NULL)
+    return *pp;
+  else
+    return NULL;
+}
+
+str_grid_patch_t* str_grid_edge_data_z_patch(str_grid_edge_data_t* edge_data, int i, int j, int k)
+{
+  int index = patch_index(edge_data, i, j, k);
+  str_grid_patch_t** pp = (str_grid_patch_t**)int_ptr_unordered_map_get(edge_data->z_patches, index);
+  if (pp != NULL)
+    return *pp;
+  else
+    return NULL;
+}
+
+bool str_grid_edge_data_next_x_patch(str_grid_edge_data_t* edge_data, int* pos, 
+                                     int* i, int* j, int* k, 
+                                     str_grid_patch_t** x_patch)
+{
+  bool result = str_grid_next_patch(edge_data->grid, pos, i, j, k);
+  if (result)
+    *x_patch = str_grid_edge_data_x_patch(edge_data, *i, *j, *k);
+  return result;
+}
+
+bool str_grid_edge_data_next_y_patch(str_grid_edge_data_t* edge_data, int* pos, 
+                                     int* i, int* j, int* k, 
+                                     str_grid_patch_t** y_patch)
+{
+  bool result = str_grid_next_patch(edge_data->grid, pos, i, j, k);
+  if (result)
+    *y_patch = str_grid_edge_data_y_patch(edge_data, *i, *j, *k);
+  return result;
+}
+
+bool str_grid_edge_data_next_z_patch(str_grid_edge_data_t* edge_data, int* pos, 
+                                     int* i, int* j, int* k, 
+                                     str_grid_patch_t** z_patch)
+{
+  bool result = str_grid_next_patch(edge_data->grid, pos, i, j, k);
+  if (result)
+    *z_patch = str_grid_edge_data_z_patch(edge_data, *i, *j, *k);
+  return result;
+}
+
+void* str_grid_edge_data_buffer(str_grid_edge_data_t* edge_data)
+{
+  return edge_data->buffer;
+}
+
+void str_grid_edge_data_set_buffer(str_grid_edge_data_t* edge_data, 
+                                   void* buffer, 
+                                   bool assume_control)
+{
+  if ((edge_data->buffer != NULL) && edge_data->owns_buffer)
+    polymec_free(edge_data->buffer);
+  edge_data->buffer = buffer;
+  edge_data->owns_buffer = assume_control;
+
+  // Point the patches at the buffer.
+  int pos = 0, l = 0, ip, jp, kp;
+  str_grid_patch_t* patch;
+  while (str_grid_edge_data_next_x_patch(edge_data, &pos, &ip, &jp, &kp, &patch))
+  {
+    int patch_offset = edge_data->patch_offsets[l];
+    patch->data = &(((real_t*)buffer)[patch_offset]);
+    ++l;
+  }
+
+  pos = 0;
+  while (str_grid_edge_data_next_y_patch(edge_data, &pos, &ip, &jp, &kp, &patch))
+  {
+    int patch_offset = edge_data->patch_offsets[l];
+    patch->data = &(((real_t*)buffer)[patch_offset]);
+    ++l;
+  }
+
+  pos = 0;
+  while (str_grid_edge_data_next_z_patch(edge_data, &pos, &ip, &jp, &kp, &patch))
+  {
+    int patch_offset = edge_data->patch_offsets[l];
+    patch->data = &(((real_t*)buffer)[patch_offset]);
+    ++l;
+  }
+}
+
---
+++ polyamri/str_grid_edge_data.h
@@ -0,0 +1,92 @@
+// Copyright (c) 2014-2016, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef POLYAMRI_STR_GRID_EDGE_DATA_H
+#define POLYAMRI_STR_GRID_EDGE_DATA_H
+
+#include "core/point.h"
+#include "polyamri/str_grid.h"
+
+// A structured grid edge data object is a collection of edge-centered patches 
+// that are associated with a structured grid in 3D space. Edge-centered patches
+// do not possess ghost values, and edges on neighboring patches overlap.
+typedef struct str_grid_edge_data_t str_grid_edge_data_t;
+
+// Creates a str_grid_edge_data object associated with the given grid, with 
+// the given number of components.
+str_grid_edge_data_t* str_grid_edge_data_new(str_grid_t* grid, 
+                                             int num_components);
+
+// Creates a str_grid_edge_data object associated with the given grid, with 
+// patches whose data is aliased to data in the given patch buffer, which is 
+// a serialized into a sequential buffer. This object does not own the data 
+// in the buffer--it only accesses it. It is up to the caller to ensure that 
+// the lifetime of the buffer exceeds that of the resulting str_grid_edge_data
+// object. NOTE: the buffer can be NULL as long as no patch data is accessed, 
+// and can be set using str_grid_edge_data_set_buffer below.
+str_grid_edge_data_t* str_grid_edge_data_with_buffer(str_grid_t* grid, 
+                                                     int num_components, 
+                                                     void* buffer);
+
+// Frees the given str_grid_edge_data object.
+void str_grid_edge_data_free(str_grid_edge_data_t* edge_data);
+
+// Returns the number of components in the str_grid_edge_data object.
+int str_grid_edge_data_num_components(str_grid_edge_data_t* edge_data);
+
+// Returns the total number of edges in the str_grid_edge_data object. 
+int str_grid_edge_data_num_edges(str_grid_edge_data_t* edge_data);
+
+// Returns an internal pointer to the given object's underlying str_grid.
+str_grid_t* str_grid_edge_data_grid(str_grid_edge_data_t* edge_data);
+
+// Given a tuple (i, j, k) identifying a patch in the underlying str_grid,
+// returns a corresponding patch containing data for x-edges, or NULL if the 
+// patch is not present in the str_grid. 
+str_grid_patch_t* str_grid_edge_data_x_patch(str_grid_edge_data_t* edge_data, int i, int j, int k);
+
+// Given a tuple (i, j, k) identifying a patch in the underlying str_grid,
+// returns a corresponding patch containing data for y-edges, or NULL if the 
+// patch is not present in the str_grid. 
+str_grid_patch_t* str_grid_edge_data_y_patch(str_grid_edge_data_t* edge_data, int i, int j, int k);
+
+// Given a tuple (i, j, k) identifying a patch in the underlying str_grid,
+// returns a corresponding patch containing data for z-edges, or NULL if the 
+// patch is not present in the str_grid. 
+str_grid_patch_t* str_grid_edge_data_z_patch(str_grid_edge_data_t* edge_data, int i, int j, int k);
+
+// Traverses the grid data, returning true if an x-edge patch was found and false if not.
+// Set *pos to 0 to reset the traversal. x_patch is set to the x-edge patch.
+bool str_grid_edge_data_next_x_patch(str_grid_edge_data_t* edge_data, int* pos, 
+                                     int* i, int* j, int* k, 
+                                     str_grid_patch_t** x_patch);
+
+// Traverses the grid data, returning true if a y-edge patch was found and false if not.
+// Set *pos to 0 to reset the traversal. y_patch is set to the y-edge patch.
+bool str_grid_edge_data_next_y_patch(str_grid_edge_data_t* edge_data, int* pos, 
+                                     int* i, int* j, int* k, 
+                                     str_grid_patch_t** y_patch);
+
+// Traverses the grid data, returning true if an z-edge patch was found and false if not.
+// Set *pos to 0 to reset the traversal. z_patch is set to the z-edge patch.
+bool str_grid_edge_data_next_z_patch(str_grid_edge_data_t* edge_data, int* pos, 
+                                     int* i, int* j, int* k, 
+                                     str_grid_patch_t** z_patch);
+
+// Returns the pointer to the underlying patch data buffer.
+void* str_grid_edge_data_buffer(str_grid_edge_data_t* edge_data);
+
+// Resets the pointer to the underlying patch data buffer, destroying or 
+// releasing all existing patch data. If assume_control is true, the 
+// str_grid_edge_data object will assume control over the buffer and free it 
+// upon destruction--otherwise it is assumed to be managed elsewhere.
+void str_grid_edge_data_set_buffer(str_grid_edge_data_t* edge_data, 
+                                   void* buffer, 
+                                   bool assume_control);
+
+#endif
+
